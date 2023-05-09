374a8ee6b28d2d65a71e96e3a32ad1c38c623f74
Brandon Bozeman <128614590+YutskiLGC@users.noreply.github.com>
2023-05-09 13:13:00 -0400

    books.js

--- src/books.js
+++ src/books.js
@@ -1,29 +1,37 @@
 function findAuthorById(authors, id) {
-  return authors.find((author) => author.id === id);
-}
+  const found = authors.find((author) => author.id === id) 
+  return found;
+};
+
+
 function findBookById(books, id) {
-  return books.find((book) => book.id === id);
+  const found = books.find((book) => book.id === id )
+  return found;
 }
+
+
+
 function partitionBooksByBorrowedStatus(books) {
-  const borrowedBooks = [];
-  const returnedBooks = [];
-  for (let i = 0; i < books.length; i++) {
-    const book = books[i];
-    if (book.borrows[0].returned === false) {
-      borrowedBooks.push(book);
-    } else {
-      returnedBooks.push(book);
-    }
-  }
-  return [borrowedBooks, returnedBooks];
+  const resultOne = books.filter((book) => book.borrows[0].returned === false );
+  const resultTwo = books.filter((book) => book.borrows[0].returned === true );
+  return [resultOne, resultTwo];
 }
+
+
+
 function getBorrowersForBook(book, accounts) {
-  const borrowers = book.borrows.map(borrow => {
-    const account = accounts.find(acc => acc.id === borrow.id);
-    return {...borrow, ...account};
-  });
-  return borrowers.slice(0, 10);
+  // return an array of ten or fewer account objects that represents the accounts given //
+  // by the IDs in the provided book's 'borrows' array //
+  return book.borrows.map((borrow) => {
+   const account = accounts.find(account => account.id === borrow.id) 
+   account.returned = borrow.returned
+   return account
+  }) .slice(0,10)
 }
+
+
+
+
 module.exports = {
   findAuthorById,
   findBookById,
